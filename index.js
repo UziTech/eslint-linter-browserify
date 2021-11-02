779f1f10f7663dfce7b48ebddb05939c23915f3d
Tony Brix <tony@brix.ninja>
2021-11-02 18:12:06 -0500

    bypass exports

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ if (typeof window === "object") {
 	window.process = require("process/browser");
 }
 
-const {Linter} = require("eslint/lib/linter/linter");
+const {Linter} = require("./node_modules/eslint/lib/linter/linter");
 
 module.exports.Linter = Linter;
