fe1cb4979b9a40cc505b924caa61a266c6fd57a9
Tony Brix <tony@brix.ninja>
2026-02-07 02:03:57 -0700

    update eslint ecmaVersion

--- eslint.config.js
+++ eslint.config.js
@@ -2,30 +2,30 @@ const js = require("@eslint/js");
 const globals = require("globals");
 
 module.exports = [
-	js.configs.recommended,
-	{
-		languageOptions: {
-				globals: {
-						...globals.browser,
-						...globals.node,
-						globalThis: "readonly",
-				},
-				parserOptions: {
-					sourceType: "module",
-					ecmaVersion: 2022,
-					ecmaFeatures: {
-						impliedStrict: true
-					}
-				},
-		},
-	},
-	{
-		ignores: [
-			"linter.js",
-			"linter.cjs",
-			"linter.min.js",
-			"linter.mjs",
-			"example/bundle.js",
-		]
-	}
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        globalThis: "readonly",
+      },
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: 2025,
+        ecmaFeatures: {
+          impliedStrict: true,
+        },
+      },
+    },
+  },
+  {
+    ignores: [
+      "linter.js",
+      "linter.cjs",
+      "linter.min.js",
+      "linter.mjs",
+      "example/bundle.js",
+    ],
+  },
 ];
