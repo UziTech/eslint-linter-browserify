33da380907afd500f97d33119082c4710e7e558b
Alexandro Paixao Marques <aleinfoit@gmail.com>
2026-08-30 17:25:04 -0400

    build: adopt @stackline rollup polyfill node

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,13 +3,9 @@ import {nodeResolve} from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import terser from "@rollup/plugin-terser";
-import alias from '@rollup/plugin-alias';
 
 function generateRollup(output) {
 	const plugins = [
-		alias({
-			entries: [{find: /^node:(.*)/, replacement: '$1'}],
-		}),
 		commonjs({
 			ignoreGlobal: true,
 			requireReturnsDefault: "preferred",
